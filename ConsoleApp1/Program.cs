4afe29596defc8150eaa8aa90a18358f88bf8a92
fankit8686 <fankit8686@gmail.com>
2018-12-12 13:06:28 +0700

    Корректировка первой части

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,44 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
+
 
 namespace Cи_Шарп_ЛР1_1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.Write("X : ");
             int x = int.Parse(Console.ReadLine());
             Console.Write("Y : ");
             int y = int.Parse(Console.ReadLine());
-            int[,] arr = new int[x,y];
-            PrintArray(arr);
-            Console.WriteLine(arr.Length);
-            Console.WriteLine("Массив с заменой столбцов");
-            Swap(arr);
-            PrintArraySwap(arr);
+            int[,] Arr = new int[x, y];
+            InitArray(Arr);
+            PrintArray(Arr);
+            Swap(Arr);
+            PrintArray(Arr);
             Console.ReadKey();
         }
-
-        public static void PrintArray(int[,] Array)
+        public static void InitArray(int[,] Array)
         {
-            var rnd = new Random();  
+            var rnd = new Random();
             for (int i = 0; i < Array.GetLength(0); i++)
             {
                 for (int j = 0; j < Array.GetLength(1); j++)
                 {
-                    Array[i,j] = rnd.Next(0, 10);                             
-                    Console.Write("{0,1}  ", Array[i,j]);
+                    Array[i, j] = rnd.Next(0, 10);                   
                 }
                 Console.WriteLine();
             }
         }
-
-        public static void PrintArraySwap(int[,] Array)
+        public static void PrintArray(int[,] Array)
         {
             for (int i = 0; i < Array.GetLength(0); i++)
             {
@@ -48,11 +41,9 @@ namespace Cи_Шарп_ЛР1_1
                 }
                 Console.WriteLine();
             }
-        }
-
+        }      
         public static void Swap(int[,] Array)
-        {
-         
+        {      
             for (int j = 0; j < Array.GetLength(1)/2  ; j++)
             {
                for (int i = 0; i < Array.GetLength(0); i++)
@@ -62,7 +53,6 @@ namespace Cи_Шарп_ЛР1_1
                     Array[i, Array.GetLength(1) - 1 - j] = temp;
                }
             }
-
         }
     }
 }
